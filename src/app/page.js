56f6159ddf08300bfb7d2aa68e830ea6b412032a
glitchnobody <58382686+glitchnobody@users.noreply.github.com>
2024-05-16 12:06:42 +0530

    autoplay

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,47 @@
+"use client";
 import Image from "next/image";
+import { useState, useRef, useEffect } from "react";
 
 export default function Home() {
+  const [isOpen, setIsOpen] = useState(true);
+
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    const handleMouseMove = () => {
+      if (videoRef.current) {
+        videoRef.current.play();
+      }
+    };
+    window.addEventListener("mousemove", handleMouseMove);
+    window.addEventListener("click", handleMouseMove);
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+      window.removeEventListener("click", handleMouseMove);
+    };
+  }, []);
+
   return (
-    <main className=" h-dvh w-full bg-black">
+    <main className=" h-dvh w-full bg-black overflow-hidden">
+      {isOpen && (
+        <div
+          onClick={() => setIsOpen(false)}
+          className="absolute top-0  text-white left-0 gap-5 w-full h-full flex flex-col items-center justify-center bg-black z-10"
+        >
+          <h1 className=" text-5xl">Are You Ready?</h1>
+          <span className="  text-gray-300 text-sm">
+            Click anywhere to enter
+          </span>
+        </div>
+      )}
       <div className="flex flex-col items-center justify-center gap-4 h-full w-full">
         <video
-          autoPlay
-          src="/projection.mp4"
-          loop
-          muted
+          ref={videoRef}
+          src="/privacywars.mp4"
           playsInline
           preload="auto"
-          className="w-full h-dvh max-h-[80dvh] object-center object-cover"
+          muted={false}
+          className="w-full aspect-video h-auto max-h-[70vh] object-contain "
         ></video>
         <button className="bg-pink-600 text-white hover:bg-pink-800 active:bg-pink-600 min-w-[200px] px-4 py-2 rounded-lg ">
           <a href="">CTA</a>
